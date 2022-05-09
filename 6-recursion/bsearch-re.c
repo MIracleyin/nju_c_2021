c852538026061b61692d888c50b6ca490630cbfa
MIracleyin <miracleyin@live.com>
2022-05-09 21:06:56 +0800

    7-re

--- 6-recursion/bsearch-re.c
+++ 6-recursion/bsearch-re.c
@@ -5,36 +5,37 @@
 
 #include <stdio.h>
 
-#define MAX 20
-int dict[MAX] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+#define MAX 10
+
+
+int BinarySearch(int key, int dict[], int low, int high);
+
 int main() {
+  int dict[MAX] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+
   int key = 0;
   scanf("%d", &key);
 
-  int index = -1;
-
-
-  int low = 0;
-  int high = MAX - 1;
-
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    if (key > dict[mid]) {
-      low = mid + 1;
-    } else if (key < dict[mid]) {
-      high = mid - 1;
-    } else { // key = dict[mid]
-      index = mid;
-      break;
-    }
-  }
-
-  if (index == -1) {
-    printf("Not fount!\n");
-  } else {
-    printf("The index of %d is %d.\n", key, index);
-  }
+  printf("%d %d", key, BinarySearch(key, dict, 0, MAX - 1));
 
   return 0;
+}
+
+int BinarySearch(int key, int dict[], int low, int high) {
+  if (low > high) {
+    return -1;
+  }
+  int mid = (low + high) / 2;
+  if (dict[mid] == key) {
+    return mid;
+  }
+  if (dict[mid] > key) {
+    // tail recursion （尾递归) 递归调用是最后一句 ex return
+    return BinarySearch(key, dict, low, mid);
+  }
+  if (dict[mid] < key) {
+    // tail recursion （尾递归) 递归调用是最后一句 ex return
+    return BinarySearch(key, dict, mid + 1, high);
+  }
 }
 
